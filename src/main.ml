03602fd9cf72b995e4b27c9c555aecaca29970da
yon <anaseto@bardinflor.perso.aquilenet.fr>
2016-02-11 19:45:59 +0100

    mis des array

--- src/main.ml
+++ src/main.ml
@@ -6,10 +6,11 @@ let get_sol data =
   { bar = 1 }
 
 let _ =
-   let file =
-     (try Sys.argv.(1) with _ -> failwith ("Specify a file name, please.")) in
-   let d = parse file in
-   Printf.printf "%s\n" (print_input d);
-   let sol = get_sol d in
-   out_sol file sol
-		 
+  let file =
+    (try Sys.argv.(1) with _ -> failwith ("Specify a file name, please.")) in
+  let d = parse file in
+  Printf.printf "%s\n" (print_input d);
+  let sol = get_sol d in
+  out_sol file sol
+
+
